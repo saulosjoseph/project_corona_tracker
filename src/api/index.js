4f39beaaa8f232850c795c1eb3e8fce3b381e4f5
ssous+1-at-721683055701 <saulo.sousa@alltaxplatform.com>
2020-05-08 11:20:02 -0300

    equalize initial data and add ecuador

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,26 @@ import axios from 'axios';
 
 const url = 'https://api.covid19api.com';
 
+const equalizeData = (country, reference) => {
+  const toAdd = reference.length - country.length;
+  const dataToAdd = reference.slice(0, toAdd);
+  const newData = dataToAdd.map((data) => ({
+    Deaths: 0,
+    Recovered: 0,
+    Confirmed: 0,
+    Date: data.date,
+  }));
+  country.map((data) => newData.push(data));
+  return newData;
+};
+
 export const fetchData = async () => {
   const brazilUrl = `${url}/dayone/country/brazil`;
   const argentinaUrl = `${url}/dayone/country/argentina`;
   const venezuelaUrl = `${url}/dayone/country/venezuela`;
   const boliviaUrl = `${url}/dayone/country/bolivia`;
   const peruUrl = `${url}/dayone/country/peru`;
+  const ecuadorUrl = `${url}/dayone/country/ecuador`;
 
   try {
     const brazilData = await axios.get(brazilUrl);
@@ -16,13 +30,15 @@ export const fetchData = async () => {
     const venezuelaData = await axios.get(venezuelaUrl);
     const boliviaData = await axios.get(boliviaUrl);
     const peruData = await axios.get(peruUrl);
+    const ecuadorData = await axios.get(ecuadorUrl);
 
     return {
       brasil: brazilData.data,
-      argentina: argentinaData.data,
-      venezuela: venezuelaData.data,
-      bolivia: boliviaData.data,
-      peru: peruData.data,
+      argentina: equalizeData(argentinaData.data, brazilData.data),
+      venezuela: equalizeData(venezuelaData.data, brazilData.data),
+      bolivia: equalizeData(boliviaData.data, brazilData.data),
+      peru: equalizeData(peruData.data, brazilData.data),
+      ecuador: equalizeData(ecuadorData.data, brazilData.data),
     };
   } catch (error) {
     return error;
